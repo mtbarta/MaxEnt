4f54e7fb287dafc5cd7467705f11f8f347a74b26
Matt <mtbarta@gmail.com>
2015-05-17 16:10:12 -0500

    fixed to-map function. clojure reads file in as lazy sequence, needed to convert lines to strings before splitting.

--- src/maxent/maxent.clj
+++ src/maxent/maxent.clj
@@ -18,35 +18,29 @@
     (helper (clojure.java.io/reader filename))))
 
 (defn to-map
-  "take a a line from a filewords and make it a map."
+  "take a a line from a file and make it a map."
   [lines]
-  (into {} (apply map vector [:text :pos :chunk] lines))
-  ;;(map #(apply
-  ;;       (->>
-  ;;        ;;(clojure.string/split % #" ")
-  ;;        (map vector [:text :pos :chunk])
-  ;;        (into {})
-  ;;        )) lines)
-  )
-(defn to-map-seq
-  [seq]
-  (map to-map seq))
-  
+  (map
+  #(zipmap [:text :pos :chunk] (clojure.string/split (apply str %) #" "))lines)
+  )  
 
 (defn window
   "create windows around the target word using a shingling pattern."
   [size filelines]
   (partition size 1 [] filelines))
 
-;;(defn create-features
-;;  "creates the features and tags from the datafile."
-;;  [filename windowsize  & features]
-;;  (->>
-;;   (lazy-file-lines filename)
-;;   (window windowsize)))
+;;still doesn't work
+(defn create-features
+  "creates the features and tags from the datafile."
+  [filename windowsize  & features]
+  (->>
+   (lazy-file-lines filename)
+   (window windowsize)))
    ;; windows are seqs of seqs of lines from the file.
-   ;;(map #(apply
+;;(map #(apply
 
-(println (to-map (seq ['(I am groot)])))
+;;(println (zipmap [:one :two :three] (clojure.string/split (apply str(take 1(lazy-file-lines "/home/matt/Documents/Projects/clojure/summarizer/resources/train.txt")))#" ")))
 
-(println(take 2 (window 3(to-map-seq(lazy-file-lines "/home/matt/Documents/Projects/clojure/summarizer/resources/train.txt")))))
+;;(println (to-map(lazy-file-lines "/home/matt/Documents/Projects/clojure/summarizer/resources/train.txt")))
+
+(println(take 2 (window 3(to-map(lazy-file-lines "/home/matt/Documents/Projects/clojure/summarizer/resources/train.txt")))))
